bd06b725ff9d90878f8548fa0a714789c683d5ef
yuval <yuvalshmuely8@gmail.com>
2024-11-16 17:37:03 +0200

    updating schedule for both files

--- Python/speedtest_test.py
+++ Python/speedtest_test.py
@@ -23,11 +23,13 @@
 
 import speedtest
 import datetime
+import time
+import schedule
 
 st = speedtest.Speedtest()
-today = datetime.datetime.now().strftime("[%d/%m/%Y/%H:%M:%S]")
 
 def speed_testing():
+    today = datetime.datetime.now().strftime("[%d/%m/%Y/%H:%M:%S]")
     print("Measuring download speed...")
     download = st.download()
     download_speed = f"Download speed: {download / 1_000_000:.2f} Mbps"
@@ -39,8 +41,19 @@ def speed_testing():
     print(upload_speed)
 
     # Return speeds as a string
-    return f"{download_speed}\n{upload_speed}"
+    # return f"{download_speed}\n{upload_speed}"
 
 # Save the output to speed_log.txt
-with open("speed_log.txt", "a") as file:
-    file.write(f"{today}\n{speed_testing()}\n")
+    with open("speed_log.txt", "a") as file:
+     file.write(f"{today}\n{download_speed}\n{upload_speed}\n")
+
+schedule.every().sunday.at("09:15").do(speed_testing)
+schedule.every().monday.at("09:15").do(speed_testing)
+schedule.every().tuesday.at("09:15").do(speed_testing) 
+schedule.every().wednesday.at("09:15").do(speed_testing)
+
+
+
+while True: 
+    schedule.run_pending()
+    time.sleep(1)
